1cdc256aa9f0cf1d8ef7db6c699b907e1654443d
TechPertz <reetnandy@gmail.com>
2021-08-12 21:50:41 +0530

    virtualenv and blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(index):
+    return HttpResponse('This is Home')
+
+def blog(index):
+    return HttpResponse('This is Blog')
